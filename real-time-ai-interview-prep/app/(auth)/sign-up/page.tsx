305e9c9ca6612a067cd6a5170563dae75bc0ff6e
Pranav Deshpande <pdeshpande540@gmail.com>
2025-05-17 13:15:30 +0530

    Created interview cards in home-page

--- real-time-ai-interview-prep/app/(auth)/sign-up/page.tsx
+++ real-time-ai-interview-prep/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AuthForm from '@/components/AuthForm'
-import {router} from 'next/navigation'
+
 import Link from 'next/link'
 
 const page = () => {
--- real-time-ai-interview-prep/app/(root)/layout.tsx
+++ real-time-ai-interview-prep/app/(root)/layout.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { ReactNode } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const Rootlayout = ({children}: {children:ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href='/' className='flex items-center gap-2'>
+        <Image src='/logo.svg' alt='logo' width={38} height={32}></Image>
+        <h2 className='text-primary-100'>Nexara</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 export default Rootlayout
